4d335620264db966cc4c08dd7b06bea3c3e4f3fa
k05m0navt <gubaydullin.dan@gmail.com>
2021-08-27 05:18:25 +0300

    Create template for Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,24 @@ import { FC } from "react";
 export interface INavbar {}
 
 const Navbar: FC<INavbar> = () => {
-    return <></>;
+    return (
+        <header className="lg:px-24 px-6 bg-white flex flex-wrap items-center justify-between lg:py-0 py-2 fixed w-screen z-10">
+            <label
+                htmlFor="menu-toggle"
+                className="pointer-cursor lg:hidden block"
+            ></label>
+            <input className="hidden" type="checkbox" id="menu-toggle" />
+
+            <div
+                className="hidden lg:flex lg:items-center lg:w-auto w-full"
+                id="menu"
+            >
+                <nav>
+                    <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0"></ul>
+                </nav>
+            </div>
+        </header>
+    );
 };
 
 export default Navbar;
