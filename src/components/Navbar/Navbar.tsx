11545078972cba9a50a9c6252ee6eab09340b0f9
k05m0navt <gubaydullin.dan@gmail.com>
2021-09-01 18:46:13 +0300

    First version of navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 import { FC } from "react";
+import { NavLink } from "react-router-dom";
+import getRoute from "../../functions/getRoute";
+import getText from "../../functions/getText";
+import Logo from "../Logo/Logo";
 
 export interface INavbar {}
 
 const Navbar: FC<INavbar> = () => {
     return (
-        <header className="lg:px-24 px-6 bg-white flex flex-wrap items-center justify-between lg:py-0 py-2 fixed w-screen z-10">
+        <header className="lg:px-24 px-6 bg-white flex flex-wrap items-center justify-between lg:py-2 py-2 fixed w-screen z-10 shadow-lg">
+            <Logo className="flex space-x-3" width="42px" height="42px" />
             <label
                 htmlFor="menu-toggle"
                 className="pointer-cursor lg:hidden block"
@@ -16,7 +21,17 @@ const Navbar: FC<INavbar> = () => {
                 id="menu"
             >
                 <nav>
-                    <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0"></ul>
+                    <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0 space-x-5">
+                        <NavLink to={getRoute("game")}>
+                            <li>{getText("navbar.game")}</li>
+                        </NavLink>
+                        <NavLink to={getRoute("dashboard")}>
+                            <li>{getText("navbar.dashboard")}</li>
+                        </NavLink>
+                        <NavLink to={getRoute("faq")}>
+                            <li>{getText("navbar.faq")}</li>
+                        </NavLink>
+                    </ul>
                 </nav>
             </div>
         </header>
