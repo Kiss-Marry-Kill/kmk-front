fd3928a04039644e03469eaeea41792175ed2c48
k05m0navt <gubaydullin.dan@gmail.com>
2021-09-03 17:13:33 +0300

    Add burger menu icon to navbar on mobiles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,31 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { NavLink } from "react-router-dom";
 import getRoute from "../../functions/getRoute";
 import getText from "../../functions/getText";
+import Icon from "../Icon/Icon";
 import Logo from "../Logo/Logo";
 
 export interface INavbar {}
 
 const Navbar: FC<INavbar> = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <header className="lg:px-24 px-6 bg-white flex flex-wrap items-center justify-between lg:py-2 py-2 fixed w-screen z-10 shadow-lg">
             <Logo className="flex space-x-3" width="42px" height="42px" />
             <label
                 htmlFor="menu-toggle"
                 className="pointer-cursor lg:hidden block"
-            ></label>
-            <input className="hidden" type="checkbox" id="menu-toggle" />
+            >
+                <Icon name="menu" width="32px" height="32px" />
+            </label>
+            <input
+                checked={isMenuOpen}
+                onChange={() => setIsMenuOpen((prev) => !prev)}
+                className="hidden"
+                type="checkbox"
+                id="menu-toggle"
+            />
 
             <div
                 className="hidden lg:flex lg:items-center lg:w-auto w-full"
