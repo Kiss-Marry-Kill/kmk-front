15eff46b30846e6598d256ef0065af5d34eaad5e
k05m0navt <gubaydullin.dan@gmail.com>
2021-09-05 19:56:05 +0300

    Template for Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export interface IButton {}
+
+const Button: FC<IButton> = () => {
+    return <></>;
+};
+
+export default Button;
---
+++ src/components/Button/index.ts
@@ -0,0 +1,4 @@
+import { memo } from "react";
+import Button, { IButton } from "./Button";
+
+export default memo<IButton>(Button);
