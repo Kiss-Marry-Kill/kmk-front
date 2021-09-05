95a9a0c1babc95f714d1deeb9fea81ad1ea7b3ac
k05m0navt <gubaydullin.dan@gmail.com>
2021-09-05 20:56:21 +0300

    Template for Dashboard page

---
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export interface IDashboardPage {}
+
+const DashboardPage: FC<IDashboardPage> = () => {
+    return <></>;
+};
+
+export default DashboardPage;
--- src/routers/NavbarRouter/NavbarRouter.tsx
+++ src/routers/NavbarRouter/NavbarRouter.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import getRoute from "../../functions/getRoute";
+import DashboardPage from "../../pages/DashboardPage/";
 import GamePage from "../../pages/GamePage";
 
 export interface INavbarRouter {}
@@ -17,7 +18,9 @@ const NavbarRouter: FC<INavbarRouter> = () => {
                 <Route path={getRoute("game")}>
                     <GamePage />
                 </Route>
-                <Route path={getRoute("dashboard")}></Route>
+                <Route path={getRoute("dashboard")}>
+                    <DashboardPage />
+                </Route>
                 <Route path={getRoute("faq")}></Route>
             </Switch>
         </>
